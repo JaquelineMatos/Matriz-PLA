9d215bc251d21a9e08c6a777f8668b5222d48866
JaquelineMatos <83770565+JaquelineMatos@users.noreply.github.com>
2021-08-30 14:29:59 -0300

    Add files via upload

---
+++ ex 2.matrizes 2.py
@@ -0,0 +1,36 @@
+def transposedtMatrix()
+def show()
+def multiplyMatrix()
+def inverseSquared()
+def sumMatrix()
+def subMatrix()
+
+print("Dadas as matrizes A, B e C, calcule (B+At) * Ci - (3 * Bt)")
+print()
+
+a = [[2, 1], [-3, 4]]
+b = [[0, -1], [2, 5]]
+c = [[3, 0], [6, 1]]
+print(f"{'Matriz A:':^18}")
+show(a)
+print(f"{'Matriz B:':^18}")
+show(b)
+print(f"{'Matriz C:':^18}")
+show(c)
+aT = transposedtMatrix(a)
+cI = inverseSquared(c)
+bT = transposedtMatrix(b)
+result = create(2, 2)
+
+print(f"{'Matriz B + At:':^18}")
+result = sumMatrix(b, aT)
+show(result)
+print(f"{'Resultado 1 * Ci:'}")
+aux = multiplyMatrix(result, cI)
+show(aux)
+print(f"{'Resultado 3 * Bt:'}")
+result = multiplyMatrix(bT, 3)
+show(result)
+print(f"{'o resultado é:'}")
+final = subMatrix(aux, result)
+show(final)
