888ea39367467579c111a4aea16581522f6d9470
JaquelineMatos <83770565+JaquelineMatos@users.noreply.github.com>
2021-08-30 14:25:56 -0300

    Add files via upload

---
+++ ex 1.matrizes 2.py
@@ -0,0 +1,29 @@
+def show()
+def sumMatrix()
+def subMatrix()
+def multiplyMatrix() 
+
+
+
+print("dadas as matrizes a,b,c calcule [a+(b-ct]*b)")
+print()
+a = [[2,1], [-3,4]]
+b = [[0,-1], [2,5]]
+c = [[3,0], [6,1]]
+print(f"{'matriz a:':^18}")
+show(a)
+print(f"{'matriz b:':^18}")
+show(b)
+print(f"{'matriz b:':^18}")
+show(c)
+
+cT = transposedtMatriz(c)
+print("resultado 1 (b -ct))")
+aux = submatrix(b, ct)
+show(aux)
+print("resultado 2 (a+(b-ct))")
+aux2=sumMatrix(a, aux)
+show(aux2)
+print("O resultado é")
+result=multiplyMatrix(aux, b)
+show(result)
